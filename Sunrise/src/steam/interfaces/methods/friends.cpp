5617862ba4475f8501da6be84ebbd83ee70c7344
ahojsvet <36peter97@gmail.com>
2026-08-24 23:26:10 +0200

    [CMake] Adjustments - Changed default player name way of retrieval - Added several CMake properties for easier compilation

--- Sunrise/src/steam/interfaces/methods/friends.cpp
+++ Sunrise/src/steam/interfaces/methods/friends.cpp
@@ -5,7 +5,11 @@ namespace sunrise::steam::interfaces::methods {
 
 /** @return Persona name from settings. It lasts for the whole process. */
 const char* persona_name([[maybe_unused]] void* self) noexcept {
+#ifdef CUSTOM_PLAYER_NAME
+    return CUSTOM_PLAYER_NAME;
+#else
     return core::settings::get().steam.user.personaName.data();
+#endif
 }
 
 } // namespace sunrise::steam::interfaces::methods
